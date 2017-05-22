f71f2addf71875c8b6b55cb970d20da5edded101
Dave Azzi <mycroc@gmail.com>
2017-05-22 10:37:50 -0400

    Added fancybox styling

--- js/snazzisweets_theme.js
+++ js/snazzisweets_theme.js
@@ -1,17 +1,20 @@
 (function ($) {
-  Drupal.behaviors.SnazziCookieJarLoad = {
+  Drupal.behaviors.SnazziSweetsLoaded = {
     attach: function (context, settings) {
       // Do your thing here.
 	  var pathname = window.location.pathname;
-	  if (pathname.startsWith('/cookies'))
+	  if (pathname.startsWith('/cookies') || pathname.startsWith('/cupcakes'))
 	  {
 		var pathitems = pathname.split('/');
 		if (pathitems.length > 2){
-			var cookieFocused = $('#node-' + pathitems[2]);
-			cookieFocused.addClass('snazzi-cookie-focused');
+			var itemFocused = $('#node-' + pathitems[2]);
+			itemFocused.addClass('snazzi-focused');
 		}
 	  }
 	  
+	  $('a[href*=#]').fancybox({	
+		
+	  });
     }
   };
 })(jQuery);

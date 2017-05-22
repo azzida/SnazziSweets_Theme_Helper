6dfcf447f0187afe27d1ccfc185c7aaffe66f325
Dave Azzi <mycroc@gmail.com>
2017-05-22 15:42:50 -0400

    Added login and styles for fancybox

--- js/snazzisweets_theme.js
+++ js/snazzisweets_theme.js
@@ -2,18 +2,11 @@
   Drupal.behaviors.SnazziSweetsLoaded = {
     attach: function (context, settings) {
       // Do your thing here.
-	  var pathname = window.location.pathname;
-	  if (pathname.startsWith('/cookies') || pathname.startsWith('/cupcakes'))
-	  {
-		var pathitems = pathname.split('/');
-		if (pathitems.length > 2){
-			var itemFocused = $('#node-' + pathitems[2]);
-			itemFocused.addClass('snazzi-focused');
-		}
-	  }
-	  
 	  $('a[href*=#]').fancybox({	
-		
+		width: '75',
+		afterLoad: function () {
+			$(".fancybox-inner").addClass("container");
+		}
 	  });
     }
   };

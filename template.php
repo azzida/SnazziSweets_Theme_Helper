5adf27d90e471a22c43c2d3a8ec33af21935d958
Azzi <azzida@city.a.ottawa.ca>
2017-06-05 09:23:12 -0400

    Consolidated latest changes

--- template.php
+++ template.php
@@ -4,4 +4,11 @@
  * The primary PHP file for this theme.
  */
 
- 
+ //Set img-responsive on our images
+function snazzisweets_theme_preprocess_image(&$variables) {
+  if(isset($variables['style_name'])) {
+    if($variables['style_name'] == 'snazzi_fancybox_banner_item') {
+     $variables['attributes']['class'][] = "img-responsive";
+		}
+	}
+}
